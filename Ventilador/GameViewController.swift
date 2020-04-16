f27e6c2d634a6bd900c3fea8ff4f3aa40d915762
Mariana Lima <mrl.lima85@gmail.com>
2020-04-16 16:44:40 -0300

    botoes funcionando liga/desliga

--- Ventilador/GameViewController.swift
+++ Ventilador/GameViewController.swift
@@ -12,84 +12,108 @@ import SceneKit
 
 class GameViewController: UIViewController {
     let scene = SCNScene(named: "art.scnassets/ship.scn")!
+    let cameraNode = SCNNode()
+    var baseVentis = SCNNode()
+    var gradeVentis = SCNNode()
+    var heliceVentis = SCNNode()
+    var botao1 = SCNNode()
+    var botao2 = SCNNode()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // create a new scene
-        
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
+       
+//        criando, adicionando e posicionando a camera
         cameraNode.camera = SCNCamera()
         scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
         
-        // create and add a light to the scene
+        // criando, adicionando e posicionando a luz
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
         lightNode.light!.type = .omni
         lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
         scene.rootNode.addChildNode(lightNode)
         
-        // create and add an ambient light to the scene
+        // criando e adicionando na cena
         let ambientLightNode = SCNNode()
         ambientLightNode.light = SCNLight()
         ambientLightNode.light!.type = .ambient
         ambientLightNode.light!.color = UIColor.darkGray
         scene.rootNode.addChildNode(ambientLightNode)
         
-        // retrieve the ship node
+        // criando e adicionando na cena as peças do ventilador
         let baseVentilador = scene.rootNode.childNode(withName: "baseVentilador reference", recursively: true)!
         let gradeVentilador = scene.rootNode.childNode(withName: "gradeVentilador reference", recursively: true)!
         let heliceVentilador = scene.rootNode.childNode(withName: "heliceVentilador reference", recursively: true)!
         let botao1Ventilador = scene.rootNode.childNode(withName: "BotaoVentilador1 reference", recursively: true)!
         let botao2Ventilador = scene.rootNode.childNode(withName: "BotaoVentilador2 reference", recursively: true)!
         
+        // escalando as peças do ventilador
         heliceVentilador.scale = SCNVector3(x: 1, y: 1, z: 1)
         botao1Ventilador.scale = SCNVector3(x: 1, y: 1, z: 1)
         botao2Ventilador.scale = SCNVector3(x: 1, y: 1, z: 1)
-        
-        heliceVentilador.rotation = SCNVector4(x: 0, y: 0, z: 0, w: 1.2)
-        baseVentilador.rotation = SCNVector4(x: 0, y: 0, z: 0, w: 1.2)
-        heliceVentilador.position = SCNVector3(x: 0, y: 0, z: 3)
-        baseVentilador.position = SCNVector3(x: 0, y: -30, z: 30)
-        
         baseVentilador.scale = SCNVector3(x: 1, y: 1, z: 1)
-        
         gradeVentilador.scale = SCNVector3(x: 0.1, y: 0.1, z: 0.1)
         
+        // rotacionando as peças do ventilador
+        heliceVentilador.rotation = SCNVector4(x: 0, y: 0, z: 0, w: 1.2)
+        baseVentilador.rotation = SCNVector4(x: 0, y: 0, z: 0, w: 1.2)
+        
+        // posicionando as peças do ventilador
+        heliceVentilador.position = SCNVector3(x: 0, y: 0, z: 3)
+        baseVentilador.position = SCNVector3(x: 0, y: -30, z: 30)
         gradeVentilador.position = SCNVector3(x: 0, y: 0, z: 0)
+        botao1Ventilador.position = SCNVector3(x: 4, y: -27, z: -36)
+        botao2Ventilador.position = SCNVector3(x: -5, y: -27, z: -36)
         
-     gradeVentilador.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 2)))
-     heliceVentilador.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 0, z: 2, duration: 0.3)))
-     
         
-        // retrieve the SCNView
+        // falando que a scnView é essa view
         let scnView = self.view as! SCNView
         
-        // set the scene to the view
+        // setando a cena da scnView
         scnView.scene = scene
         
-        // allows the user to manipulate the camera
+        // deixando o usuario mexer na camera
         scnView.allowsCameraControl = true
         
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
-        
+        // adicionando as peças como filhas
         gradeVentilador.addChildNode(heliceVentilador)
         gradeVentilador.addChildNode(baseVentilador)
         baseVentilador.addChildNode(botao1Ventilador)
         baseVentilador.addChildNode(botao2Ventilador)
         
+        // setando os nomes das peças
+        baseVentilador.name = "baseVentiladorName"
+        baseVentis = baseVentilador
+        gradeVentilador.name = "gradeVentiladorName"
+        gradeVentis = gradeVentilador
+        heliceVentilador.name = "heliceVentiladorName"
+        heliceVentis = heliceVentilador
+        botao1Ventilador.name = "botao1Name"
+        botao1 = botao1Ventilador
+        botao2Ventilador.name = "botao2Name"
+        botao2 = botao2Ventilador
         
-        // add a tap gesture recognizer
+        
+        // adicionando o tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         scnView.addGestureRecognizer(tapGesture)
         
     }
+    func animacoesFuncionando() {
+        if botao1.name == "botao1Name" {
+           gradeVentis.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 2)))
+            heliceVentis.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 0, z: 2, duration: 0.3)))
+            botao1.position = SCNVector3(x: 4, y: -28, z: -36)
+            botao2.position = SCNVector3(x: -5, y: -27, z: -36)
+            gradeVentis.isPaused = false
+            heliceVentis.isPaused = false
+        }
+    }
+    
     @objc
     func handleTap(_ gestureRecognize: UIGestureRecognizer) {
+        
         // retrieve the SCNView
         let scnView = self.view as! SCNView
         
@@ -97,28 +121,16 @@ class GameViewController: UIViewController {
         let p = gestureRecognize.location(in: scnView)
         let hitResults = scnView.hitTest(p, options: [:])
         // check that we clicked on at least one object
-        
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result = hitResults[0]
-            // get its material
-            let material = result.node.geometry!.firstMaterial!
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                material.emission.contents = UIColor.red
-                SCNTransaction.commit()
+        for i in hitResults {
+            if i.node.parent?.name == "botao1Name"{
+                 self.animacoesFuncionando()
+            } else if i.node.parent?.name == "botao2Name"{
+                gradeVentis.isPaused = true
+                heliceVentis.isPaused = true
+                botao2.position = SCNVector3(x: -5, y: -28, z: -36)
+                botao1.position = SCNVector3(x: 4, y: -27, z: -36)
+                
             }
-            
-            material.emission.contents = UIColor.black
-            
-            SCNTransaction.commit()
         }
     }
     
